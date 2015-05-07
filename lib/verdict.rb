1fa844f501270c9b8439b9fb4975f10e4cd03813
Steven Rockarts <stevenrockarts@gmail.com>
2015-05-07 02:55:19 +0000

    Verdict with passing tests, needs refactoring

--- lib/verdict.rb
+++ lib/verdict.rb
@@ -5,11 +5,19 @@ class Verdict
   end
   
   def tabulate
-    opinion_counts = Hash.new 0
+    opinion_counts = Hash.new(0)
 
     @seating_arrangement.flatten.each do |opinion|
       opinion_counts[opinion] += 1
     end
-    return opinion_counts.max_by{|k,v| v}[0]
+    
+    max_opinion = opinion_counts.values.max
+    prevailing_opinion = opinion_counts.select { |k, v| v == max_opinion }.keys
+    
+    if prevailing_opinion.count > 1
+      return :push
+    else
+      return prevailing_opinion[0]
+    end
   end
 end
--- spec/verdict_spec.rb
+++ spec/verdict_spec.rb
@@ -14,9 +14,33 @@ describe Verdict do
       it "should tabulate results" do
         result = verdict.tabulate()
         expect(result).to eq(:hard)
-        
       end
-      
+    end
+    
+    context "majority of opinionated people think gif is a soft g" do
+      let(:verdict) { Verdict.new( [
+               [:soft, :soft, :soft],
+               [:hard, :none, :soft],
+               [:hard, :soft, :hard],
+              ]) }
+              
+      it "should tabulate results" do
+        result = verdict.tabulate()
+        expect(result).to eq(:soft)
+      end
+    end
+    
+    context "push when opinions are equal" do
+      let(:verdict) { Verdict.new( [
+               [:soft, :soft, :soft],
+               [:soft, :none, :hard],
+               [:hard, :hard, :hard],
+              ]) }
+              
+      it "should tabulate results" do
+        result = verdict.tabulate()
+        expect(result).to eq(:push)
+      end
     end
   end
 end
